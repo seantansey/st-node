2b61eb38ccf4c879856f8be0d055bca481b13d0e
Sean Tansey <sean.d.tansey@gmail.com>
2022-05-07 00:04:50 -0600

    changes migration pkg

---
+++ migrations/1651901543078_messages.js
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = pgm => {
+    pgm.createTable('messages', {
+        id: 'id',
+        name: {
+            type: 'varchar(30)',
+            notNull: true
+        },
+        email: {
+            type: 'varchar(30)',
+            notNull: true
+        },
+        subject: {
+            type: 'varchar(30)',
+            notNull: true
+        },
+        message: {
+            type: 'text',
+            notNull: true
+        },
+        date: {
+            type: 'timestamp',
+            notNull: true,
+            default: pgm.func('current_timestamp')
+        }
+    })
+};
+
+exports.down = pgm => {};
