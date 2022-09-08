19d10a33cfaede747aa7b420913fa20f275de14d
Sean Tansey <sean.d.tansey@gmail.com>
2022-09-07 21:11:57 -0600

    deletes console.log

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,6 @@
 const validURLs = [ 'https://www.seantansey.com' ]
 
 const domainCheck = (req, res, next) => {
-    console.log(req.headers.host)
     if (validURLs.includes(req.headers.host)) next()
     else res.status(403).send('You do not have permission to access this information')
 }
